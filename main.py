e0b53f904017c823f9397e184184a766bdc7b9a0
LarsNickel <166020084+LarsNickel@users.noreply.github.com>
2024-04-17 13:34:10 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def macheEtwas(num: number):
-    pass
 def Vorhang():
     while pins.digital_read_pin(DigitalPin.P13) == 0:
         ContinuousServo.spin_other_way_with_speed(AnalogPin.P1, 50)
@@ -10,6 +8,7 @@ def Vorhang():
 def Helligkeit():
     led.enable(True)
     serial.write_value("Helligkeit", input.light_level())
+    led.enable(False)
     return input.light_level()
 def Feuchtigkeit():
     global feuchte
@@ -19,12 +18,23 @@ def Pumpe():
     pass
 feuchte = 0
 pins.set_pull(DigitalPin.P13, PinPullMode.PULL_UP)
-lumi = 0
+pins.digital_write_pin(DigitalPin.P10, 0)
 led.enable(False)
+lumi = 0
+feuchte = 0
 
 def on_forever():
-    Helligkeit()
-    Feuchtigkeit()
     Vorhang()
-    macheEtwas(1)
 basic.forever(on_forever)
+
+def on_forever2():
+    while input.button_is_pressed(Button.A):
+        ContinuousServo.spin_one_way_with_speed(AnalogPin.P1, 50)
+    ContinuousServo.turn_off_motor(DigitalPin.P1)
+basic.forever(on_forever2)
+
+def on_forever3():
+    while input.button_is_pressed(Button.B):
+        ContinuousServo.spin_other_way_with_speed(AnalogPin.P1, 50)
+    ContinuousServo.turn_off_motor(DigitalPin.P1)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -10,21 +10,36 @@ function Vorhang () {
 function Helligkeit () {
     led.enable(true)
     serial.writeValue("Helligkeit", input.lightLevel())
+    led.enable(false)
     return input.lightLevel()
 }
 function Feuchtigkeit () {
     feuchte = pins.analogReadPin(AnalogPin.P2)
     serial.writeValue("Feuchtigkeit", feuchte)
+    return feuchte
 }
 function Pumpe () {
 	
 }
+function Temperatur () {
+    temp = 3 / 1023 * pins.analogReadPin(AnalogPin.P0) / 0.01
+    serial.writeValue("Temperatur", temp)
+    return temp
+}
+let temp = 0
 let feuchte = 0
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
-let lumi = 0
+pins.digitalWritePin(DigitalPin.P10, 0)
 led.enable(false)
+let lumi = 0
+feuchte = 0
+temp = 0
 basic.forever(function () {
-    Helligkeit()
-    Feuchtigkeit()
-    Vorhang()
+    if (Temperatur() >= 21) {
+        pins.digitalWritePin(DigitalPin.P10, 1)
+    } else {
+        if (Temperatur() <= 20) {
+            pins.digitalWritePin(DigitalPin.P10, 0)
+        }
+    }
 })
