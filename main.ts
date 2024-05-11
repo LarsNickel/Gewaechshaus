b97fa297a280f893c6195414b5f436ff8cb9353f
LarsNickel <166020084+LarsNickel@users.noreply.github.com>
2024-05-11 12:14:29 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P10, 0)
+    mode_flag = 0
+})
+function Licht (num: number) {
+    if (num <= 50) {
+        pins.digitalWritePin(DigitalPin.P9, 1)
+    } else {
+        if (num >= 70) {
+            pins.digitalWritePin(DigitalPin.P9, 0)
+        }
+    }
+}
+function Luefter (num: number) {
+    led.enable(false)
+    if (num >= 25) {
+        pins.digitalWritePin(DigitalPin.P10, 1)
+    } else {
+        if (num <= 22) {
+            pins.digitalWritePin(DigitalPin.P10, 0)
+        }
+    }
+}
 function Vorhang (num: number) {
-    if (num >= 130) {
+    if (num >= 230 && vorhang_oben == 1) {
         ContinuousServo.spin_one_way_with_speed(AnalogPin.P1, 50)
         basic.pause(2000)
         ContinuousServo.turn_off_motor(DigitalPin.P1)
+        vorhang_oben = 0
     } else {
         if (num <= 75) {
             while (pins.digitalReadPin(DigitalPin.P13) == 0) {
                 ContinuousServo.spin_other_way_with_speed(AnalogPin.P1, 50)
             }
             ContinuousServo.turn_off_motor(DigitalPin.P1)
+            vorhang_oben = 1
         }
     }
 }
@@ -19,6 +44,9 @@ function Helligkeit () {
     led.enable(false)
     return lumi
 }
+input.onButtonPressed(Button.B, function () {
+    mode_flag = 1
+})
 function Feuchtigkeit () {
     feuchte = pins.analogReadPin(AnalogPin.P2)
     return feuchte
@@ -28,6 +56,7 @@ function Vorhang_init () {
         ContinuousServo.spin_other_way_with_speed(AnalogPin.P1, 50)
     }
     ContinuousServo.turn_off_motor(DigitalPin.P1)
+    vorhang_oben = 1
 }
 function Pumpe (num: number) {
     if (num >= 380) {
@@ -40,8 +69,11 @@ function Pumpe (num: number) {
 }
 function Temperatur () {
     temp = 3 / 1023 * pins.analogReadPin(AnalogPin.P0) / 0.01
+    serial.writeValue("Temperatur", temp)
     return temp
 }
+let mode_flag = 0
+let vorhang_oben = 0
 let temp = 0
 let feuchte = 0
 let lumi = 0
@@ -51,15 +83,16 @@ led.enable(false)
 lumi = 0
 feuchte = 0
 temp = 0
+vorhang_oben = 0
+mode_flag = 0
 Vorhang_init()
 basic.forever(function () {
-    if (Temperatur() >= 25) {
-        pins.digitalWritePin(DigitalPin.P10, 1)
-    } else {
-        if (Temperatur() <= 22) {
-            pins.digitalWritePin(DigitalPin.P10, 0)
-        }
-    }
     Pumpe(Feuchtigkeit())
-    Vorhang(Helligkeit())
+    if (mode_flag == 0) {
+        Luefter(Temperatur())
+    }
+    if (mode_flag == 1) {
+        Vorhang(Helligkeit())
+        Licht(Helligkeit())
+    }
 })
